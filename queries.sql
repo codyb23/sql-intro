ec66737473cbd0442061f9a31dace94b7590297c
CodyBanks <codydouglasbanks@gmail.com>
2020-05-27 15:31:23 -0400

    Steps 6,7,8 Finished

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-1.
+--1.
 
 CREATE TABLE "Employees"
 (
@@ -10,7 +10,7 @@ CREATE TABLE "Employees"
   "Id" SERIAL PRIMARY KEY
 );
 
-2.
+--2.
 
 INSERT INTO "Employees"
   ("FullName", "Salary", "JobPosition", "PhoneExtension",
@@ -66,39 +66,133 @@ INSERT INTO "Employees"
 VALUES
   ('Gavin Stark', '950', 'Software Developer', '727-895-9037', 'No');
 
-3.
+--3.
 
 SELECT "FullName"
 FROM "Employees";
 
-4.
+--4.
 
 SELECT "FullName", "PhoneExtension"
 From "Employees"
 WHERE "IsPartTime" = 'No';
 
 
-5.
+--5.
 
 INSERT INTO "Employees"
   ("FullName", "Salary", "JobPosition", "PhoneExtension",
   "IsPartTime")
 VALUES
-  ('Cherry GoldMan', '850', 'Software Developer', '727-657-9154', 'Yes');
+  ('Cherry GoldMan', '950', 'Software Developer', '727-657-9154', 'Yes');
 
 
-6.
+--6.
 
-UPDATE "Employees" SET "Salary" = '500' Where "Salary" = '400'
-UPDATE "Employees" SET "Salary" = '500' Where "Salary" = '300'
-UPDATE "Employees" SET "Salary" = '500' Where "Salary" = '600'
-UPDATE "Employees" SET "Salary" = '500' Where "Salary" = '450'
+UPDATE "Employees" SET "Salary" = '500' Where "JobPosition" = 'Cook'
 
-7.
+
+--7.
 
 DELETE FROM "Employees" WHERE "FullName" = 'Lazy Larry';
 
-8.
+--8.
 
 ALTER TABLE "Employees" ADD COLUMN "ParkingSpot" VARCHAR
 (10);
+
+-- Foreign Keys
+--1. 
+
+CREATE TABLE "Departments"
+(
+  "Id" SERIAL PRIMARY KEY,
+  "DepartmentName" TEXT,
+  "Building" TEXT
+);
+
+--2. 
+
+ALTER TABLE "Employees" ADD COLUMN "DepartmentId" 
+INTEGER NULL REFERENCES "Departments"
+("Id");
+
+--3.
+
+CREATE TABLE "Orders"
+(
+  "Id" SERIAL PRIMARY KEY,
+  "OrderNumber" TEXT,
+  "DatePlaced" TIMESTAMP,
+  "Email" Text
+);
+
+CREATE TABLE "Products"
+(
+  "Id" SERIAL PRIMARY KEY,
+  "Price" DOUBLE PRECISION,
+  "Name" TEXT,
+  "Description" TEXT,
+  "QuantityInStock" INT
+);
+
+--4.
+
+CREATE TABLE "ProductOrders"
+(
+  "Id" SERIAL PRIMARY KEY,
+  "ProductId" INTEGER REFERENCES "Products" ("Id"),
+  "OrderId" INTEGER REFERENCES "Orders" ("Id"),
+  "OrderQuantity" INTEGER
+);
+
+--5.
+
+INSERT INTO "Departments"
+  ("DepartmentName", "Building")
+VALUES
+  ('Development', 'Main');
+
+INSERT INTO "Departments"
+  ("DepartmentName", "Building")
+VALUES
+  ('Marketing', 'North');
+
+--6.
+
+INSERT INTO "Employees"
+  ("FullName", "Salary", "JobPosition", "PhoneExtension",
+  "IsPartTime", "DepartmentId")
+VALUES
+  ('Tim Smith', '40000', 'Programmer', '123', 'false', '1');
+
+INSERT INTO "Employees"
+  ("FullName", "Salary", "JobPosition", "PhoneExtension",
+  "IsPartTime", "DepartmentId")
+VALUES
+  ('Barbara', '80000', 'Manager', '234', 'false', '1');
+
+INSERT INTO "Employees"
+  ("FullName", "Salary", "JobPosition", "PhoneExtension",
+  "IsPartTime", "DepartmentId")
+VALUES
+  ('Tom Jones', '32000', 'Admin', '456', 'true', '2');
+
+--7.
+
+INSERT INTO "Products"
+  ("Price", "Name", "Description", "QuantityInStock")
+VALUES
+  ('12.45', 'Widget', 'The Original Widget', '100');
+
+INSERT INTO "Products"
+  ("Price", "Name", "Description", "QuantityInStock")
+VALUES
+  ('99.99', 'Flowbee', 'Perfect for haircuts', '3');
+
+--8.
+
+INSERT INTO "Orders"
+  ("OrderNumber", "DatePlaced", "Email")
+VALUES
+  ('X529', '2020-01-01 16:55:00', 'person@example.com');
